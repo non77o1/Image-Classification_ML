2f85ed232cb8649e211f66eef9acabd31afd0fe7
Hneen <none7703@gmail.com>
2023-11-25 07:18:30 +0300

    all done

--- server/server.py
+++ server/server.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 @app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
     # well do img classification here by the model we trained
-    return 'Hi'
+    img_data = request.form['image_data']
+    
+    # tern the function reslut into a json response
+    response = jsonify(util.classify_image(img_data))
+    
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
+    print("Starting Python Flask Server For Sport Celebrity Image Classification...")
+    util.load_saved_artifacts()
     app.run()
-    # util.load_saved_artifacts()
-    # app.run()
